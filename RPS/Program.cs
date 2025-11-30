c8e4a0467ead166b9a4f82d8eeca84d7ff1339fe
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-11-30 15:10:00 +0100

    Change welcome text

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -1,6 +1,6 @@
 ﻿// // See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Let's play Rock Paper Scissors!");
+Console.WriteLine("Let's play Rock Paper Scissors, ok?");
 
 Console.WriteLine("Player 1, provide sign (rock/paper/scissors)");
 string firstSign = Console.ReadLine()!;
